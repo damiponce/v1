41945887a51cc6b98fe574d72d4a73ea32f3d5b9
Damián Ponce <dami.ponce8@gmail.com>
2021-10-14 17:05:03 -0300

    updated photography page

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,43 +1,81 @@
 import fs from 'fs';
 import path from 'path';
 const sizeOf = require('image-size');
+import { ExifParserFactory } from 'ts-exif-parser';
 export interface PicType {
-    fileName: string;
-    id: string;
-    fullPath: string;
-    width: number;
-    height: number;
-    ratio: number;
-    index: number;
-    orientation?: number;
+   fileName: string;
+   id: string;
+   fullPath: string;
+   width: number;
+   height: number;
+   ratio: number;
+   index: number;
+   orientation?: number;
+   dateTaken: number;
 }
 
 export interface GroupPicsType {
-    [index: string]: PicType;
+   [index: string]: PicType;
 }
 
-export function getAllPhotos(): PicType[] {
-    const picsDirectory = path.join(process.cwd(), 'public/images/photos');
-    // Get file names
-    const fileNames = fs.readdirSync(picsDirectory);
-    const allPhotosData = fileNames.map((fileName, index) => {
-        // Remove extension from file name to get id
-        const id = fileName.replace(/\.(png|jpe?g|svg)$/i, '');
-        // Join full path of the item
-        const fullPath = '/images/photos/' + fileName;
-        // Get dimensions and ratio of item
-        const dimensions = sizeOf('public/images/photos/' + fileName);
-        const width = dimensions.width;
-        const height = dimensions.height;
-        const ratio =
+export async function getAllPhotos(): Promise<PicType[]> {
+   var buffers: any[] = [];
+
+   const picsDirectory = path.join(process.cwd(), 'public/images/photos');
+   // Get file names
+   const fileNames = fs.readdirSync(picsDirectory);
+   const allPhotosData = await Promise.all(
+      fileNames.map(async (fileName, index) => {
+         // Remove extension from file name to get id
+         const id = fileName.replace(/\.(png|jpe?g|svg)$/i, '');
+         // Join full path of the item
+         const fullPath = '/images/photos/' + fileName;
+         // Get dimensions and ratio of item
+         const dimensions = sizeOf('public/images/photos/' + fileName);
+         const width = dimensions.width;
+         const height = dimensions.height;
+         const ratio =
             dimensions.orientation % 2 ||
             dimensions.type != 'jpg' ||
             (dimensions.type == 'jpg' && dimensions.orientation == null)
-                ? width / height
-                : height / width;
-        const orientation = dimensions.orientation || null;
-        // Combine the data and return it
-        return {
+               ? width / height
+               : height / width;
+         const orientation = dimensions.orientation || null;
+         await fs.promises
+            .open(picsDirectory + '/' + fileName, 'r')
+            .then(async (fd) => {
+               // if (status) {
+               //     console.log(status.message);
+               //     return;
+               // }
+
+               const BUFFER_LENGTH = 65635;
+
+               var buffer = Buffer.alloc(BUFFER_LENGTH);
+               await fd
+                  .read(buffer, 0, BUFFER_LENGTH, 0)
+                  .then(async (value) => {
+                     const parser = ExifParserFactory.create(buffer);
+                     parser.enableReturnTags(true);
+                     parser.enableTagNames(true);
+                     try {
+                        const result = parser.parse();
+                        buffers.push(result.tags?.DateTimeOriginal);
+                     } catch (err) {
+                        // buffers.push(JSON.stringify(err));
+                        console.log(err);
+                     }
+
+                     //  buffers.push([
+                     //      buffer.toString('utf8', 0, value.bytesRead),
+                     //      result,
+                     //  ]);
+                  });
+            });
+         // Combine the data and return it
+         var dateTaken =
+            typeof buffers[index] !== 'undefined' ? buffers[index] : 0;
+         return {
             fileName,
             id,
             fullPath,
@@ -46,67 +84,70 @@ export function getAllPhotos(): PicType[] {
             ratio,
             index,
             orientation,
-        };
-    });
+            dateTaken,
+         };
+      })
+   );
 
-    return allPhotosData;
+   return allPhotosData;
 }
 
 export function getAllDesigns(): GroupPicsType {
-    const picsDirectory = path.join(process.cwd(), 'public/images/designs');
-    // Get file names
-    const fileNames = fs.readdirSync(picsDirectory);
-    const allDesignsData = fileNames.map((fileName, index) => {
-        // Remove extension from file name to get id
-        const id = fileName.replace(/\.(png|jpe?g|svg)$/i, '');
-        // Join full path of the item
-        const fullPath = '/images/designs/' + fileName;
-        // Get dimensions and ratio of item
-        const dimensions = sizeOf('public/images/designs/' + fileName);
-        const width = dimensions.width;
-        const height = dimensions.height;
-        const ratio =
-            dimensions.orientation % 2 ||
-            dimensions.type != 'jpg' ||
-            (dimensions.type == 'jpg' && dimensions.orientation == null)
-                ? width / height
-                : height / width;
-        // Combine the data and return it
-        return {
-            fileName,
-            id,
-            fullPath,
-            width,
-            height,
-            ratio,
-            index,
-        };
-    });
+   const picsDirectory = path.join(process.cwd(), 'public/images/designs');
+   // Get file names
+   const fileNames = fs.readdirSync(picsDirectory);
+   const allDesignsData = fileNames.map((fileName, index) => {
+      // Remove extension from file name to get id
+      const id = fileName.replace(/\.(png|jpe?g|svg)$/i, '');
+      // Join full path of the item
+      const fullPath = '/images/designs/' + fileName;
+      // Get dimensions and ratio of item
+      const dimensions = sizeOf('public/images/designs/' + fileName);
+      const width = dimensions.width;
+      const height = dimensions.height;
+      const ratio =
+         dimensions.orientation % 2 ||
+         dimensions.type != 'jpg' ||
+         (dimensions.type == 'jpg' && dimensions.orientation == null)
+            ? width / height
+            : height / width;
+      // Combine the data and return it
+      return {
+         fileName,
+         id,
+         fullPath,
+         width,
+         height,
+         ratio,
+         index,
+      };
+   });
 
-    const arr = [];
+   const arr = [];
 
-    for (let i = 0; i < allDesignsData.length; i++) {
-        let tempArr = [allDesignsData[i]['fileName'], allDesignsData[i]];
-        arr.push(tempArr);
-    }
+   for (let i = 0; i < allDesignsData.length; i++) {
+      let tempArr = [allDesignsData[i]['fileName'], allDesignsData[i]];
+      arr.push(tempArr);
+   }
 
-    return Object.fromEntries(arr);
+   return Object.fromEntries(arr);
 }
 
 import { ImageLoader, ImageLoaderProps } from 'next/image';
+import { useState } from 'react';
 
 export const customLoader: ImageLoader = (props: ImageLoaderProps) => {
-    // return `https://example.com/${src}?w=${width}&q=${quality || 75}`
-    return `https://cdn.statically.io/img/damiponce.github.io/${
-        props.src
-    }?w=${1000}&q=${props.quality || 75}`;
+   // return `https://example.com/${src}?w=${width}&q=${quality || 75}`
+   return `https://cdn.statically.io/img/damiponce.github.io/${
+      props.src
+   }?w=${1000}&q=${props.quality || 75}`;
 };
 export const fullResCustomLoader: ImageLoader = (props: ImageLoaderProps) => {
-    // return `https://example.com/${src}?w=${width}&q=${quality || 75}`
-    return `https://damiponce.github.io/${props.src}?&q=${100}`;
+   // return `https://example.com/${src}?w=${width}&q=${quality || 75}`
+   return `https://damiponce.github.io/${props.src}?&q=${100}`;
 };
 
 export const blurCustomLoader = (src: string) => {
-    // return `https://example.com/${src}?w=${width}&q=${quality || 75}`
-    return `https://cdn.statically.io/img/damiponce.github.io/${src}?w=${10}&q=${75}`;
+   // return `https://example.com/${src}?w=${width}&q=${quality || 75}`
+   return `https://cdn.statically.io/img/damiponce.github.io/${src}?w=${10}&q=${75}`;
 };
--- src/pages/photography.tsx
+++ src/pages/photography.tsx
@@ -10,114 +10,124 @@ import Masonry from 'react-masonry-css';
 import styles from '../styles/photography.module.css';
 
 export const getStaticProps: GetStaticProps = async (context) => {
-    var allPicturesData = getAllPhotos();
-    allPicturesData = shuffle(allPicturesData);
-
-    return {
-        props: {
-            allPicturesData,
-        },
-    };
+   var allPicturesData = await getAllPhotos();
+   //  allPicturesData = shuffle(allPicturesData);
+   allPicturesData.sort(function (a, b) {
+      return b.dateTaken - a.dateTaken;
+   });
+   return {
+      props: {
+         allPicturesData,
+      },
+   };
 };
 
 function shuffle(array: PicType[]) {
-    var currentIndex = array.length,
-        temporaryValue,
-        randomIndex;
+   var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
 
-    while (0 !== currentIndex) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
+   while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    console.log(array);
-    return array;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+   }
+   console.log(array);
+   return array;
 }
 
 export default function Photography({
-    allPicturesData,
+   allPicturesData,
 }: {
-    allPicturesData: PicType[];
+   allPicturesData: PicType[];
 }) {
-    const [viewer, setViewer] = useState<PicType[] | null>();
-    const [index, setIndex] = useState<number>(69);
+   console.log('====================================');
+   console.log(
+      allPicturesData.map((pic) => {
+         return pic.dateTaken;
+      })
+   );
+   console.log('====================================');
 
-    // Disables scroll when viewer is open
-    useEffect(() => {
-        viewer
-            ? (document.body.style.overflow = 'hidden')
-            : (document.body.style.overflow = 'unset');
-        return;
-    }, [viewer]);
+   const [viewer, setViewer] = useState<PicType[] | null>();
+   const [index, setIndex] = useState<number>(69);
 
-    return (
-        <Layout>
-            <Head>
-                <title>Photography - Damián Ponce</title>
-                <meta
-                    name='viewport'
-                    content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
-                />
-            </Head>
-            {viewer ? (
-                <Viewer
-                    onClick={() => setViewer(null)}
-                    pic={viewer}
-                    index={index}
-                />
-            ) : null}
-            <div className='section'>
-                <div className='section-title'>
-                    <div className='big-title'>fo•to•gra•fí•a</div>
-                    <div className='big-title-spell'>sustantivo</div>
-                    {/* /fəˈtɒɡ.rə.fi/ */}
-                    <div className='definitions'>
-                        1. El arte de congelar el tiempo en una imagen
-                    </div>
-                    <div className='separator' />
-                </div>
-                <div className='section-spacer' />
-                <div className='section-intro'>
-                    <p>
-                        La fotografía es la herramienta que en ocasiones me
-                        ayuda a comunicar historias y sentimientos libremente. A
-                        veces me encuentro sacandole fotos a objetos cuando me
-                        llama la atención una luz o simplemente cuando quiero
-                        compartirlo con otras personas.
-                    </p>
-                    <p>
-                        Desde una piedra hasta una nube rara, cualquier foto
-                        puede tener una historia detrás, y eso es lo que me
-                        encanta de este arte.
-                    </p>
-                </div>
+   // Disables scroll when viewer is open
+   useEffect(() => {
+      viewer
+         ? (document.body.style.overflow = 'hidden')
+         : (document.body.style.overflow = 'unset');
+      return;
+   }, [viewer]);
+
+   return (
+      <Layout>
+         <Head>
+            <title>Photography - Damián Ponce</title>
+            <meta
+               name="viewport"
+               content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+            />
+         </Head>
+         {viewer ? (
+            <Viewer
+               onClick={() => setViewer(null)}
+               pic={viewer}
+               index={index}
+            />
+         ) : null}
+         <div className="section">
+            <div className="section-title">
+               <div className="big-title">fo•to•gra•fí•a</div>
+               <div className="big-title-spell">sustantivo</div>
+               {/* /fəˈtɒɡ.rə.fi/ */}
+               <div className="definitions">
+                  1. El arte de congelar el tiempo en una imagen
+               </div>
+               <div className="separator" />
             </div>
+            <div className="section-spacer" />
+            <div className="section-intro">
+               <p>
+                  La fotografía es la herramienta que en ocasiones me ayuda a
+                  comunicar historias y sentimientos libremente. A veces me
+                  encuentro sacandole fotos a objetos cuando me llama la
+                  atención una luz o simplemente cuando quiero compartirlo con
+                  otras personas.
+               </p>
+               <p>
+                  Desde una piedra hasta una nube rara, cualquier foto puede
+                  tener una historia detrás, y eso es lo que me encanta de este
+                  arte.
+               </p>
+            </div>
+         </div>
 
-            <Masonry
-                breakpointCols={{ default: 3, 1000: 2, 650: 1 }}
-                className='masonry-grid'
-                columnClassName='masonry-grid_column'
-            >
-                {allPicturesData.map((value: PicType, index: number) => (
-                    <div key={value.index} className={styles.card}>
-                        <Image
-                            onClick={() => (
-                                setViewer(allPicturesData), setIndex(index)
-                            )}
-                            className={styles.image + ' no-touch'}
-                            loader={customLoader}
-                            src={value.fullPath}
-                            alt={value.id}
-                            width={650}
-                            height={650 / value.ratio}
-                            quality={100}
-                        />
-                    </div>
-                ))}
-            </Masonry>
-        </Layout>
-    );
+         <Masonry
+            breakpointCols={{ default: 3, 1000: 2, 650: 1 }}
+            className="masonry-grid"
+            columnClassName="masonry-grid_column"
+         >
+            {allPicturesData.map((value: PicType, index: number) => (
+               <div key={value.index} className={styles.card}>
+                  <Image
+                     onClick={() => (
+                        setViewer(allPicturesData), setIndex(index)
+                     )}
+                     className={styles.image + ' no-touch'}
+                     loader={customLoader}
+                     src={value.fullPath}
+                     alt={value.id}
+                     width={650}
+                     height={650 / value.ratio}
+                     quality={100}
+                  />
+               </div>
+            ))}
+         </Masonry>
+      </Layout>
+   );
 }
